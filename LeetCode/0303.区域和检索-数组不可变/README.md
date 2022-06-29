e9c7d207ec949575506bc9690d286e677565960b
Feafly <409435808@qq.com>
2022-06-29 22:46:45 +0800

    Add better method

--- LeetCode/0303.区域和检索-数组不可变/README.md
+++ LeetCode/0303.区域和检索-数组不可变/README.md
@@ -30,3 +30,40 @@ private:
 };
 
 ```
+
+## labuladong的解法：“前缀和”数组
+
+准备一个“前缀和”数组，为方便计算，“前缀和”数组第一个值为0，大小为`nums`大小加1
+
+“前缀和”数组中，第i个值的含义是：`nums`数组\[0, i\)的和
+
+当需要\[left, right\]区间和的时候，就相当于是\[0, right+1\)区间减去\[0, left\)区间
+
+即：`_pre[right + 1] - _pre[left]`
+
+## 源码
+
+```c++
+class NumArray {
+public:
+    NumArray(vector<int>& nums)
+        : _nums(nums)
+    {
+        _pre.resize(_nums.size() + 1);
+        _pre[0] = 0;
+        for (size_t i = 1; i < _pre.size(); ++i) {
+            _pre[i] = _pre[i - 1] + _nums[i - 1];
+        }
+    }
+
+    int sumRange(int left, int right)
+    {
+        return _pre[right + 1] - _pre[left];
+    }
+
+private:
+    vector<int>& _nums;
+    vector<int> _pre;
+};
+
+```
--- LeetCode/0303.区域和检索-数组不可变/main.h
+++ LeetCode/0303.区域和检索-数组不可变/main.h
@@ -9,19 +9,26 @@ public:
     NumArray(vector<int>& nums)
         : _nums(nums)
     {
+        _pre.resize(_nums.size() + 1);
+        _pre[0] = 0;
+        for (size_t i = 1; i < _pre.size(); ++i) {
+            _pre[i] = _pre[i - 1] + _nums[i - 1];
+        }
     }
 
     int sumRange(int left, int right)
     {
-        int cnt = 0;
-        for (int i = left; i <= right; ++i) {
-            cnt += _nums[i];
-        }
-        return cnt;
+        // int cnt = 0;
+        // for (int i = left; i <= right; ++i) {
+        //     cnt += _nums[i];
+        // }
+        // return cnt;
+        return _pre[right + 1] - _pre[left];
     }
 
 private:
     vector<int>& _nums;
+    vector<int> _pre;
 };
 
 /**
