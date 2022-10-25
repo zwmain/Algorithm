1ec8f735cd7b4b54d0c4d22ed4ec3a268f25e154
Feafly <409435808@qq.com>
2022-10-25 22:41:17 +0800

    Add solution for lt0704

--- LeetCode/0704.二分查找/main.cpp
+++ LeetCode/0704.二分查找/main.cpp
@@ -2,7 +2,20 @@
 #include "Utils.hpp"
 #include <iostream>
 
+#ifndef INPUT_PATH
+#define INPUT_PATH "./input.txt"
+#endif
+
 int main()
 {
+    auto input = zwn::inputIntArr(INPUT_PATH);
+    vector<int> index = { 9, 2, 5 };
+    Solution s;
+    for (int i = 0; i < input.size(); ++i) {
+        auto res = s.search(input[i], index[i]);
+        cout << res << endl;
+        res = s.search2(input[i], index[i]);
+        cout << res << endl;
+    }
     return 0;
 }
--- LeetCode/0704.二分查找/main.h
+++ LeetCode/0704.二分查找/main.h
@@ -4,7 +4,45 @@ using namespace std;
 
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+    int search(vector<int>& nums, int target)
+    {
+        int left = 0;
+        // 左闭右闭区间
+        int right = nums.size() - 1;
+        // 当左闭右闭区间时，左右是可以相等的
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int cur = nums[mid];
+            if (cur == target) {
+                return mid;
+            } else if (cur > target) {
+                // 当右边为闭合时，right需要mid-1
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
 
+    int search2(vector<int>& nums, int target)
+    {
+        int left = 0;
+        // 左闭右开区间
+        int right = nums.size();
+        // 当左闭右开时，左右不能相等
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int cur = nums[mid];
+            if (cur == target) {
+                return mid;
+            } else if (cur > target) {
+                // 当左闭右开时，right值就是mid，不需要mid-1
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
     }
 };
