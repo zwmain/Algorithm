30c9662274b4c020dc849e5de2947e7a2c38facd
Feafly <409435808@qq.com>
2022-10-26 21:39:14 +0800

    Add solution 3 for lt0027

--- LeetCode/0027.移除元素/main.h
+++ LeetCode/0027.移除元素/main.h
@@ -39,4 +39,23 @@ public:
         }
         return length;
     }
+    // 双指针
+    // 删除元素的基本思想是：将目标元素后面的元素前移，将快指针指向新数组中的元素
+    // 慢指针指向旧数组中应该被替换的位置
+    int removeElement2(vector<int>& nums, int val)
+    {
+        size_t slow = 0; // 指向需要被替换的位置
+        size_t fast = 0; // 指向需要保留的位置
+        for (fast; fast < nums.size(); ++fast) {
+            // 只有当fast位置不等于目标值时，才会替换前面的位置
+            // 当fast位置等于目标值时，跳过，slow位置没有被替换，因此不后移
+            if (nums[fast] != val) {
+                // 只有当fast不等于目标值时才会发生替换，++slow
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            // 不管是不是等于目标值，fast会一直后移
+        }
+        return slow;
+    }
 };
