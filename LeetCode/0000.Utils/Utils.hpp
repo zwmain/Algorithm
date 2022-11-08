56d8b90f195ac45ee33dcf574297b613056a4413
Feafly <409435808@qq.com>
2022-11-08 20:01:28 +0800

    Add TreeNode

--- LeetCode/0000.Utils/Utils.hpp
+++ LeetCode/0000.Utils/Utils.hpp
@@ -39,6 +39,34 @@ struct ListNode {
     }
 };
 
+/**
+ * @brief 二叉树节点结构体
+ *
+ */
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode()
+        : val(0)
+        , left(nullptr)
+        , right(nullptr)
+    {
+    }
+    TreeNode(int x)
+        : val(x)
+        , left(nullptr)
+        , right(nullptr)
+    {
+    }
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x)
+        , left(left)
+        , right(right)
+    {
+    }
+};
+
 namespace zwn {
 
 /**
