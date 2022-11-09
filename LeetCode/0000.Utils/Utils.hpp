928388a88facc7f1391321aa55e9c0d7090b42bf
Feafly <409435808@qq.com>
2022-11-09 21:18:23 +0800

    Add N-ary tree type

--- LeetCode/0000.Utils/Utils.hpp
+++ LeetCode/0000.Utils/Utils.hpp
@@ -67,6 +67,29 @@ struct TreeNode {
     }
 };
 
+/**
+ * @brief N叉数
+ *
+ */
+class Node {
+public:
+    int val;
+    std::vector<Node*> children;
+
+    Node() { }
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, std::vector<Node*> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
 namespace zwn {
 
 /**
