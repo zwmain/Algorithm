4aadd98ac97fba4c295c840e67e3f0ac3a3e1e64
ZhangWeinan <weinan.zhang@oi-smart.com>
2022-11-13 00:15:44 +0800

    Add solution for lt0106

---
+++ LeetCode/0106.从中序与后序遍历序列构造二叉树/README.md
@@ -0,0 +1,5 @@
+# 106. 从中序与后序遍历序列构造二叉树
+
+[106. 从中序与后序遍历序列构造二叉树](https://leetcode.cn/problems/construct-binary-tree-from-inorder-and-postorder-traversal/)
+
+
---
+++ LeetCode/0106.从中序与后序遍历序列构造二叉树/main.cpp
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "Utils.hpp"
+
+#if !defined(INPUT_PATH)
+#define INPUT_PATH "./input.txt"
+#endif // INPUT_PATH
+
+int main()
+{
+    string fp(INPUT_PATH);
+    // todo
+    return 0;
+}
