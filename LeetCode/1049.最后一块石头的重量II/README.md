9136584d5d8c71ebda798a3c4ba08e8a3b9929bd
Feafly <409435808@qq.com>
2022-12-07 21:33:14 +0800

    Add solution for lt1049

---
+++ LeetCode/1049.最后一块石头的重量II/README.md
@@ -0,0 +1,9 @@
+# 1049.最后一块石头的重量II
+
+链接：[1049.最后一块石头的重量II](https://leetcode.cn/problems/last-stone-weight-ii/)
+
+
+
+
+
+
---
+++ LeetCode/1049.最后一块石头的重量II/main.cpp
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "Utils.hpp"
+#include <iostream>
+
+#if !defined(INPUT_PATH)
+#define INPUT_PATH "./input.txt"
+#endif // INPUT_PATH
+
+#if !defined(INPUT_DIR)
+#define INPUT_DIR "./"
+#endif // INPUT_PATH
+
+int main()
+{
+    string fp(INPUT_PATH);
+    auto input = zwn::inputIntArr(fp);
+    for (auto& arr : input) {
+        Solution s;
+        auto res = s.lastStoneWeightII(arr);
+        cout << res << endl;
+    }
+    return 0;
+}
