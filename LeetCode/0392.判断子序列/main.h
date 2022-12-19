bd7d567d52d3b6f8f2743dd3064a27755ea51875
Feafly <409435808@qq.com>
2022-12-19 21:28:18 +0800

    Add solution for lt0392

---
+++ LeetCode/0392.判断子序列/main.h
@@ -0,0 +1,38 @@
+#include "Utils.hpp"
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t)
+    {
+        // dp[i][j] 表示以下标i-1为结尾的字符串s，和以下标j-1为结尾的字符串t，相同子序列的长度为dp[i][j]。初始化更方便。
+        // 递推公式：
+        // if (s[i - 1] == t[j - 1])，那么dp[i][j] = dp[i - 1][j - 1] + 1;，因为找到了一个相同的字符，相同子序列长度自然要在dp[i-1][j-1]的基础上加1
+        // if (s[i - 1] != t[j - 1])，此时相当于t要删除元素，t如果把当前元素t[j - 1]删除，那么dp[i][j] 的数值就是 看s[i - 1]与 t[j - 2]的比较结果了，即：dp[i][j] = dp[i][j - 1];
+        // 初始化：
+        // 从递推公式可以看出dp[i][j]都是依赖于dp[i - 1][j - 1] 和 dp[i][j - 1]，所以dp[0][0]和dp[i][0]是一定要初始化的。
+        // dp[i][0] 表示以下标i-1为结尾的字符串，与空字符串的相同子序列长度，所以为0. dp[0][j]同理。
+        // 遍历顺序：从上到下，从左到右
+        if (s.size() > t.size()) {
+            return false;
+        }
+        if (s.empty()) {
+            return true;
+        }
+        vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
+        for (int i = 1; i <= s.size(); ++i) {
+            for (int j = 1; j <= t.size(); ++j) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp.back().back() == s.size();
+    }
+};
