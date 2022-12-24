be1d1079f4e98b50b64c4fb45093587271a5a3c8
Feafly <409435808@qq.com>
2022-12-24 18:12:59 +0800

    Add solution for lt0084

---
+++ LeetCode/0084.柱状图中最大的矩形/README.md
@@ -0,0 +1,19 @@
+# 0084.柱状图中最大的矩形
+
+链接：[0084.柱状图中最大的矩形][(https://leetcode.cn/problems/largest-rectangle-in-histogram/)
+
+参考：[代码随想录](https://programmercarl.com/0084.柱状图中最大的矩形.html)
+
+```c++
+
+
+```
+
+
+
+
+
+
+
+
+
---
+++ LeetCode/0084.柱状图中最大的矩形/main.cpp
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "Utils.hpp"
+#include <iostream>
+
+#if !defined(INPUT_PATH)
+#define INPUT_PATH "./input.txt"
+#endif // INPUT_PATH
+
+int main()
+{
+    string fp(INPUT_PATH);
+    auto input = zwn::inputIntArr(fp);
+    for (size_t i = 0; i < input.size(); i += 1) {
+        Solution s;
+        auto res = s.largestRectangleArea(input[i]);
+        cout << res << endl;
+    }
+    return 0;
+}
